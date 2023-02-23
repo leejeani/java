760390f5bb922de4de12c0847e224e9b959d6530
leejeani <jeanman.lee@gmail.com>
2023-02-23 17:03:45 +0900

    Update P68.java
    update

--- day02/src/variable/P68.java
+++ day02/src/variable/P68.java
@@ -10,10 +10,6 @@ public class P68 {
 		int a = 10;
 		System.out.println(s2 + a);
 
-		int n1 = 10;
-		String n2 = String.valueOf(n1);
-		// String n2 = n1+"";
-
 		Random r = new Random();
 		int rndNum = r.nextInt(45)+1;
 		System.out.println(rndNum);
