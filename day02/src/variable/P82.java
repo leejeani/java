a4993df285e0e014730440eea8a6050cdd544680
User <User@DESKTOP-FRI26IN>
2023-02-24 08:36:21 +0900

    test2

--- day02/src/variable/P82.java
+++ day02/src/variable/P82.java
@@ -4,7 +4,8 @@ public class P82 {
 
 	public static void main(String[] args) {
 		int score = 79;
-		
+		int a = 10;
+		int b = 10;
 		String grade = (score >= 90) ? "A" : (score >=80) ? "B" : "F" ;
 		
 		System.out.println(grade);
