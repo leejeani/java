666f1983f00f863b3727148192c8daa9af15d4a4
User <User@DESKTOP-FRI26IN>
2023-02-27 09:05:06 +0900

    test

--- day03/src/array/Hello.java
+++ day03/src/array/Hello.java
@@ -4,17 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub4
-		int test = 100;
-		// TODO Auto-generated method stub\
-		int d = 1000000;
-		int a = 10;
-		int b = 100;
-		int dddd = 100;
-		int cc = 100; 
 		
-		int kkk = 100;
-		
-		int aaaaa = 100;
 	}
 
 }
