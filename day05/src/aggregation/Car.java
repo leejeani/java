da2bbb6cdfa604b9caa8bb6b7aaa3ec349cf15c5
jeanman.lee@gmail.com <jeanman.lee@gmail.com>
2023-03-02 16:07:09 +0900

    0302

---
+++ day05/src/aggregation/Car.java
@@ -0,0 +1,34 @@
+package aggregation;
+
+public class Car {
+	public String name;
+	public String color;
+	public int size;
+	
+	// 생성자. (constructor)
+	
+	
+	public Car() {
+		this.name = "k1";
+		this.color = "red";
+		this.size = 1000;
+	}
+	
+	public Car(String name, String color, int size) {
+		this.name = name;
+		this.color = color;
+		this.size = size;
+	}
+	
+	@Override
+	public String toString() {
+		return "Car [name=" + name + ", color=" + color + ", size=" + size + "]";
+	}
+
+	public void go() {
+		System.out.printf("%s, %s Go Car ...",this.name,this.color);
+	}
+	public void stop() {
+		System.out.printf("%s, %s Stop Car ...",this.name,this.color);
+	}
+}
