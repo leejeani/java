da2170cb77371e5b48740e6800d60a5bb1f40ac9
jeanman.lee@gmail.com <jeanman.lee@gmail.com>
2023-03-07 13:04:10 +0900

    0307

---
+++ day08/src/remote/Human.java
@@ -0,0 +1,15 @@
+package remote;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Human {
+	public static void main(String args []) {
+		Remote remote = new Audio();
+		remote.turnOff();
+		remote.turnOn();
+		
+		
+		
+	}
+}
